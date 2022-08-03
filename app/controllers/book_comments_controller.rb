cdad870cb7fd92e4770ead47f38e55ee5e0cc810
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2022-08-03 13:44:25 +0000

    [start] 応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
+    @books = Book.find(params[:book_id])
+    @book_comment = BookComment.new
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
   end
    
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @books = Book.find(params[:book_id])
+    @book_comment.destroy
+    @book_comment = BookComment.new
   end
 
   private
