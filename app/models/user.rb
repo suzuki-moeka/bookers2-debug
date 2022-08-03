cb45848b6690246e0ed4108e9faf6a875f6f25cf
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2022-08-03 08:00:00 +0000

    [finish] 応用課題5

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
- has_many :followings, through: :relationships, source: :followed
- has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
@@ -25,16 +25,31 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
-def follow(user_id)
-  relationships.create(followed_id: user_id)
-end
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
 
-def unfollow(user_id)
-  relationships.find_by(followed_id: user_id).destroy
-end
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
 
-def following?(user)
-  followings.include?(user)
-end
+  def following?(user)
+    followings.include?(user)
+  end
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+
+  end
 
 end
