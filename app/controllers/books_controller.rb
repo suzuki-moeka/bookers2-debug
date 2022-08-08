31c4f54dfda86677f53448166c15553834c9cb63
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2022-08-08 01:33:40 +0000

    [finish] 応用課題9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,25 +5,23 @@ class BooksController < ApplicationController
   def show
     @books = Book.all
     @book = Book.find(params[:id])
+     unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+       current_user.view_counts.create(book_id: @book.id)
+     end
     @book_new = Book.new
     @user = @book.user
     @book_comment = BookComment.new
-    @book_detail = Book.find(params[:id])
-     unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
-       current_user.view_counts.create(book_id: @book_detail.id)
-     end
   end
 
 
   def index
+    to  = Time.current.at_beginning_of_day
+    from  = (to - 6.day).at_end_of_day
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
+      a.favorites.where(created_at: from...to).size
+    }
     @book = Book.new
-    to  = Time.current.at_end_of_day
-    from  = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
-    @book_detail = Book.find(params[:id])
-     unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
-       current_user.view_counts.create(book_id: @book_detail.id)
-     end
   end
 
   def create
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,14 +4,14 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
   has_many :view_counts, dependent: :destroy
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?","#{word}")
@@ -25,5 +25,5 @@ class Book < ApplicationRecord
       @book = Book.all
     end
   end
-  
+
 end
